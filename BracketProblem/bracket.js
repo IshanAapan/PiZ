5f3491f2a3189a113a92a5cfbef15f1e7754b299
Ishan <ishanaapan02@gmail.com>
2024-06-13 12:27:03 +0530

    After Completion of Bracket Problem

--- BracketProblem/bracket.js
+++ BracketProblem/bracket.js
@@ -1,85 +1,39 @@
 console.log("Welcome to Bracket Problem");
 
+document.getElementById("number").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById("myBtn").click();
+  }
+});
 
 const myFun = () => {
   console.log("Inside the myFun");
-  let inputElem = document.getElementById("number").value;
-  if (inputElem == "") {
+  let inputElem = document.getElementById("number").value.trim();
+  if (inputElem === "") {
     alert("Please Enter Valid input");
     return;
   }
   let elements = inputElem.split(",");
   console.log("Output", elements);
 
+  let result = "";
+  let stack = [];
 
-  // let result = "";
-  // const stack = [];
+  for (let i = 0; i < elements.length; i++) {
+    while (stack.length > 0 && !elements[i].startsWith(stack[stack.length - 1])) {
+      stack.pop();
+      result += "]";
+    }
 
-  // elements.forEach(element => {
-  //   const levels = element.split('.').length - 1;
-
-  //   while (stack.length > levels) {
-  //     result += "]";
-  //     stack.pop();
-  //   }
-
-  //   // Open brackets for new level
-  //   if (stack.length < levels) {
-  //     result += "[";
-  //     stack.push("[");
-  //   }
-
-  //   // Add an empty bracket at the current level
-  //   result += "[]";
-  // });
-
-  // // Close all remaining open brackets
-  // while (stack.length) {
-  //   result += "]";
-  //   stack.pop();
-  // }
-
-  let result = '';
-  let currentDepth = 0;
-
-  elements.forEach(element => {
-      const parts = element.split('.');
-      const depth = parts.length - 1;
-
-      // Close brackets if the current depth is higher than the new depth
-      while (currentDepth > depth) {
-          result += ']';
-          currentDepth -= 1;
-      }
-
-      // Open new brackets if the new depth is higher
-      while (currentDepth < depth) {
-          result += '[';
-          currentDepth += 1;
-      }
-
-      // Add an empty bracket for the current element
-      result += '[]';
-  });
-
-  // Close any remaining open brackets
-  while (currentDepth > 0) {
-      result += ']';
-      currentDepth -= 1;
+    stack.push(elements[i]);
+    result += "[";
   }
 
-  // return result;
-document.getElementById("output").innerHTML=result
-}
+  while (stack.length > 0) {
+    stack.pop();
+    result += "]";
+  }
 
-
-
-  // return result;
-
-
-// const inputString = "1,1.1,1.1.1,1.1.2,1.1.3,2.1,2.1.1,2.1.2";
-// const output = createBrackets(inputString);
-// console.log(output);
-
-
-// };
+  console.log("Result", result)
+  document.getElementById("output").innerHTML = result;
+};
